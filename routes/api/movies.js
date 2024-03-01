0704094492b4a94727354928511a76691edbbd8b
Mary Danyliuk <marydanyliuk@gmail.com>
2024-03-01 15:05:10 +0200

    fix api

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -11,16 +11,16 @@ const ctrl = require("../../controllers/movies");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll);
+// router.get("/", ctrl.getAll);
 
-// router.get("/", async (req, res, next) => {
-//   return res.status(200).json({
-//     title: "Express Testing",
-//     message: "The app is working properly!",
-//   });
-//   // const result = await movies.listMovies();
-//   // return res.status(200).json(result);
-// });
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
+  // const result = await movies.listMovies();
+  // return res.status(200).json(result);
+});
 
 module.exports = router;
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -5,12 +5,12 @@ const users = require("../../models/user");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  //   return res.status(200).json({
-  //     title: "Express Testing",
-  //     message: "The app is working properly!",
-  //   });
-  const result = await users.listUsers();
-  return res.status(200).json(result);
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
+  // const result = await users.listUsers();
+  // return res.status(200).json(result);
 });
 
 router.post("/users/signup", async (req, res, next) => {
