a8b28eb89bfec93f204e89f5fcf9606aff602930
Mary Danyliuk <marydanyliuk@gmail.com>
2024-03-02 13:45:44 +0200

    upd index.js in models

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const sendEmail = require("./sendEmail");
+const handleMongooseError = require("./handleMongooseError");
 
 module.exports = {
   HttpError,
   ctrlWrapper,
   sendEmail,
+  handleMongooseError,
 };
